541d737130d909731f2c0717bbe6592e5b57560b
Gaberfahmy <67122811+Gaberfahmy@users.noreply.github.com>
2020-07-14 17:53:55 +0200

    Add files via upload

---
+++ start.py
@@ -0,0 +1,57 @@
+import os
+os.system ("clear")
+
+print ("Welcome to my script..!")
+
+virous = ("venom☠️💀👺👹👑🔥👾💣")
+
+virouss = ("Goodbye....!🖤☠️🔥👺👹👾👑💀💣")
+
+virousss = ("Fuckyou🔥👺👹👾👑💀💣☠️🖤💩👽💍🤖😈🔥😈👿👽💩💣💀👑👾👹👺🔥")
+
+viroussss = ("HackerWorld💣💀👑👑👾👹👺🔥👻🤖😈👿👿👽💩☠️🔥👺👹💍💎👻💀💣👑")
+
+virousssss = ("proffesorelhaker💣💀👑👾👹👺💣💀🤖😈👿👽👽👽☠️☠️💩💩🔥👺👹👾👑💀💣")
+
+print ("1-Virous")
+
+print ("2-Virous(2)")
+
+print ("3-Virous(3)")
+
+print ("3-Virous(4)")
+
+print ("4-Virous(5)")
+
+select = input ("Select Number: ")
+
+if select == "1" :
+    import os
+    os.system ("clear")
+    print (virous *10000)
+    print ("Now copy this virous and send it to the victim on what's app..!")
+
+if select == "2" :
+   import os
+   os.system ("clear")
+   print (virouss *10000)
+   print ("Now copy this virous and send it to the victim on what's app")
+
+if select == "3" :
+   import os
+   os.system ("clear")
+   print (virousss *10000)
+   print ("Now copy this virous and send it to the victim on what's app..!")
+
+if select == "4" :
+   import os
+   os.system ("clear")
+   print (viroussss *10000)
+   print ("Now copy this virous and send it to the victim on what's app..!")
+
+if select == "5" :
+   import os
+   os.system ("clear")
+   print (virousssss *10000)
+   print ("Now copy this virous and send it to the victim on what's app..!")
+
